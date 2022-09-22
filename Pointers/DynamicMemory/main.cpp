a3232bfe73d3b8f397814823da9ebfea1d5114af
BardakTrash <104521716+BardakTrash@users.noreply.github.com>
2022-09-22 17:55:42 +0300

    Repo-Code-C

--- Pointers/DynamicMemory/main.cpp
+++ Pointers/DynamicMemory/main.cpp
@@ -1,16 +1,29 @@
 #include<iostream>
 using namespace std;
 
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
 void main()
+//Repo-Code-C
 {
 	setlocale(LC_ALL, "");
 	int n;
 	cout << "¬ведите размер массива:"; cin >> n;
 	int* arr = new int[n];
+	FillRand(arr, n);
+	Print(arr, n);
+}
+
+void FillRand(int arr[], const int n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % 100;
 	}
+}
+void Print(int arr[], const int n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
